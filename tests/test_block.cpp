0e623309db48127ec289b37bd3d0b32684660171
amso <marcin119a@gmail.com>
2023-04-19 22:48:46 +0200

    tests for blocks

--- tests/test_block.cpp
+++ tests/test_block.cpp
@@ -24,3 +24,27 @@ BOOST_AUTO_TEST_CASE(block_union_to_for_tree_blocks) {
 
 }
 
+BOOST_AUTO_TEST_CASE(block_union_to_two_blocks) {
+    Block<int> block1(1, "actg");
+    Block<int> block2(2, "actg");
+    std::map<int, Block<int>&> blocks = {{1, block1}, {2, block2}};
+    int flank = 1;
+    int reverse = -1;
+    block2.unionto(block1, blocks, reverse, flank);
+
+    BOOST_TEST(block1.orient == 1);
+    BOOST_TEST(block2.orient == -1);
+}
+
+BOOST_AUTO_TEST_CASE(block_union_mid_to_three_blocks) {
+    Block<int> block1(1, "actg");
+    Block<int> block2(2, "actg");
+    Block<int> block3(3, "actg");
+    std::map<int, Block<int>&> blocks = {{1, block1}, {2, block2}, {3, block3}};
+
+    block2.unionto(block1, blocks, 1, 1);
+    block3.uniontoMidst(block1, blocks, 1,1);
+
+    BOOST_TEST(block1.order(blocks) < block3.order(blocks));
+    BOOST_TEST(block3.order(blocks) < block2.order(blocks));
+}
